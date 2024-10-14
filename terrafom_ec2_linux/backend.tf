7fdbe38b0728b938e2b25c791ee05437e030345c
sibanando <sibhanayak@hotmail.com>
2024-10-14 12:41:34 +0530

    s3_backend addedd

--- terrafom_ec2_linux/backend.tf
+++ terrafom_ec2_linux/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "testayush"
     key    = "jenkins/terraform.tfstate"
     region = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }
--- terrafom_ec2_linux/main.tf
+++ terrafom_ec2_linux/main.tf
@@ -84,3 +84,21 @@ module "ec2_instance" {
     Environment = "dev"
   }
 }
+
+#S3 backend
+#resource "aws_s3_bucket" "s3_bucket" {
+#  bucket = "testayush" # change this
+#}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
