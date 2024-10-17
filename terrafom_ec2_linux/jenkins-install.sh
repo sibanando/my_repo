1737b3affe5bf5bf0b59aaa742c5f626950c30d9
sibanando <30410459+sibanando@users.noreply.github.com>
2024-10-17 07:38:53 +0530

    Update jenkins-install.sh

--- terrafom_ec2_linux/jenkins-install.sh
+++ terrafom_ec2_linux/jenkins-install.sh
@@ -5,39 +5,32 @@ sudo apt update -y
 sudo apt install openjdk-17-jre -y
 sudo apt install openjdk-17-jdk -y
 java --version
-
 # Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+https://pkg.jenkins.io/debian binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install jenkins -y
-
-# Installing Docker 
+# Installing Docker
 sudo apt update -y
 sudo apt install docker.io -y
 sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
-
 # If you don't want to install Jenkins, you can create a container of Jenkins
 # docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins/jenkins:lts
-
 # Run Docker Container of Sonarqube
-docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
-#docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
-
-
+#docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
 # Installing AWS CLI
 #!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
-
 # Installing Kubectl
 #!/bin/bash
 sudo apt update
@@ -46,21 +39,17 @@ sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client
-
-
 # Installing eksctl
 #! /bin/bash
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-
 # Installing Terraform
 #!/bin/bash
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
-
 # Installing Trivy
 #!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
@@ -68,8 +57,18 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-k
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt update
 sudo apt install trivy -y
-
-
 # Intalling Helm
 #! /bin/bash
 sudo snap install helm --classic
+# EVN variable
+export ACCESS_KEY="<>"
+export SECRET_KEY="<>"
+export REGION="<>"
+# EKS setup
+eksctl create cluster --name hackathon-k8s --region us-west-2 --node-type t2.medium --zones us-west-2a,us-west-2b
+aws eks update-kubeconfig --region us-west-2 --name hackathon-k8s
+#Prometheus and Grafana
+helm repo add stable https://charts.helm.sh/stable
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create namespace prometheus
+helm install stable prometheus-community/kube-prometheus-stack -n prometheus
